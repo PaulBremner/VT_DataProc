f663419222f2df686739ef265bba0eabb0ba8050
PaulBremner <king.speen@gmail.com>
2023-04-26 17:54:56 +0100

    Data grab and timingfix

--- Project/Data/DataJSONfileProc.py
+++ Project/Data/DataJSONfileProc.py
@@ -4,19 +4,67 @@ import glob
 jsonfiles = glob.glob('FixedData/*.json')
 
 #print(jsonfiles[0])
+cols = ['condition', 'tagsNoRTL', 'tagsSelfRTL', 'tagsRobotRTL', 'clearedTags', 'totalTime']
+taggingdf = pd.DataFrame(columns = cols)
 
-df = pd.read_json(jsonfiles[0], lines=True)
-pd.set_option('display.max_columns', None)
-#print(df.head())
+for jsonfile in jsonfiles:
+#jsonfile = jsonfiles[0]
+    df = pd.read_json(jsonfile, lines=True)
+    pd.set_option('display.max_columns', None)
+    #print(df.head())
 
-df_hazard = pd.read_json('ObstacleData\obstacle_hazard_data.json')
+    #df_hazard = pd.read_json('ObstacleData\obstacle_hazard_data.json')
 
-#print(df_hazard.head())
-hazardDict = df_hazard.to_dict()
+    #print(df_hazard.head())
+    #hazardDict = df_hazard.to_dict()
 
-dataDict = df.to_dict()
+    dataDict = df.to_dict()
+    #create a df with cols = ['condition', 'tagsNoRTL', 'tagsSelfRTL', 'tagsRobotRTL']
+    participantData = {}
+    participantData['condition'] = jsonfile[10:13]
+    participantData['tagsNoRTL'] = 0
+    participantData['tagsSelfRTL'] = 0
+    participantData['tagsRobotRTL'] = 0
+    participantData['clearedTags'] = 0
+    #print(dataDict['rtlObject'])
+    for rtl in dataDict['rtlObject'].values():
+        if 'none' in rtl:
+            participantData['tagsNoRTL'] += 1
+        elif 'self' in rtl:
+            participantData['tagsSelfRTL'] += 1
+        elif 'Robot' in rtl:
+            participantData['tagsRobotRTL'] += 1
+    #print(dataDict['name'].values())
 
-#print(dataDict['name'].values())
+    for tag in dataDict['tag'].values():
+        if tag == [0.0,0.0,0.0]:
+            participantData['clearedTags'] += 1
+
+    participantData['totalTime'] = dataDict['timestamp'][len(dataDict['timestamp'])-1]
+
+    taggingdf.loc[len(taggingdf)] = participantData
+
+#print(taggingdf.head())
+
+
+jsonfiles = glob.glob('FixedTimeData/*.json')
+#jsonfile = jsonfiles[0]
+
+#print(jsonfile)
+cols = ['condition', 'timeSelfRTL', 'timeRobotRTL']
+timingdf = pd.DataFrame(columns = cols)
+
+for jsonfile in jsonfiles:
+#jsonfile = jsonfiles[0]
+    df = pd.read_json(jsonfile, lines=True)
+    timingDict = df.to_dict()
+    participantData = {}
+    participantData['condition'] = jsonfile[14:17]
+    participantData['timeSelfRTL'] = timingDict['selfRTLTime'][len(timingDict['selfRTLTime'])-2]
+    participantData['timeRobotRTL'] = timingDict['robotRTLTime'][len(timingDict['robotRTLTime'])-2]
+    timingdf.loc[len(timingdf)] = participantData
+
+print(timingdf.head())
 
 #step through each thing they have tagged and log the tags to a dict with the object name as key, that way the final object tag state is stored
 #then go through that dict and check in hazard list dict for what the tag should be, if it is non-hazardous or doesn't match log the error
--- Project/Data/jsonFixerTime.py
+++ Project/Data/jsonFixerTime.py
@@ -1,11 +1,11 @@
 import glob
 
 jsonfiles = glob.glob('TimeData/*.json')
-
+#print(jsonfiles[0][8:])
 
 for jsonfile in jsonfiles:
     with open(jsonfile, "r") as file:
         line = file.read().replace('}', '}\n')
 
-    with open('FixedData/' + jsonfile, 'w') as file:
+    with open('FixedTimeData/' + jsonfile[9:], 'w') as file:
         file.write(line)
